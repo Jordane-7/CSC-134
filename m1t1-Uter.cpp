62869c41ff4c2183aa67096cdfd6ba77d25f0065
Jordane <uterjordane@gmail.com>
2026-01-14 16:18:36 +0000

    Added Variable

--- m1t1-Uter.cpp
+++ m1t1-Uter.cpp
@@ -10,5 +10,13 @@ int main() {
     cout << "Hello CSC 134!" << endl; // E N D L
     cout << "How's it going?" << endl; 
     cout << endl; // another new line
+    string name; // new box to hold name
+    cout << "Whats's your name? ";
+    cin >> name; // read in their answer
+    // finally, say hi
+    cout << "Well hi, " << name << "!" << endl;
+
+
+
     return 0; // no errors
 }

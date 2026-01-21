3c75efcd1557d77ce3944e509a85662ab7db7c1e
Jordane <uterjordane@gmail.com>
2026-01-21 16:09:05 +0000

    m1lab

--- m1lab1.cpp
+++ m1lab1.cpp
@@ -1,5 +1,5 @@
 // CSC 134
-// M1LAB1
+// M1LAB1 - selling items
 // Jordane
 // 1/21/2025
 
@@ -7,5 +7,21 @@
 using namespace std;
 
 int main() {
+    // declare vairables
+    string item_name = "Burger";
+    int item_count = 100;           // how many we have
+    double item_cost = 3.50;        // price per item
+    double total_cost;
+
+    // Do the work
+    cout << "Welcome to the " << item_name << " store." << endl;
+    cout << "We have " << item_count << " " << item_name << "s" << endl;
+    cout << "They cost $" << item_cost << " each" << endl;
+
+    // Total price
+    total_cost = item_count * item_cost;
+
+    cout << "To buy all of the m it will ocst $" << total_cost << endl;
+
     return 0;
 }

716c364e5001ad1de3481acb8225d5be207cd4d5
Jordane <uterjordane@gmail.com>
2026-01-21 16:14:34 +0000

    Finished

--- m1lab1.cpp
+++ m1lab1.cpp
@@ -4,14 +4,18 @@
 // 1/21/2025
 
 #include <iostream>
+#include <iomanip> // for decimal places
 using namespace std;
 
 int main() {
     // declare vairables
     string item_name = "Burger";
     int item_count = 100;           // how many we have
-    double item_cost = 3.50;        // price per item
+    double item_cost = 2.53;        // price per item
     double total_cost;
+
+    // Set to 2 decimal places - requires <iomanip>
+    cout << setprecision(2) << fixed;
 
     // Do the work
     cout << "Welcome to the " << item_name << " store." << endl;
@@ -21,7 +25,10 @@ int main() {
     // Total price
     total_cost = item_count * item_cost;
 
-    cout << "To buy all of the m it will ocst $" << total_cost << endl;
+    cout << "To buy all of them it will cost $" << total_cost << endl;
+
+    
+
 
     return 0;
 }

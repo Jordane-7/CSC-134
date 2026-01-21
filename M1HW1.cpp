282c6c49e060db1dab7631398fd85e02f9b7430e
Jordane <uterjordane@gmail.com>
2026-01-21 16:36:22 +0000

    Started

---
+++ M1HW1.cpp
@@ -0,0 +1,20 @@
+// CSC 134
+// M1LAB1 - selling items
+// Jordane
+// 1/21/2025
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    string movie_name = "Friday";
+    int year_made = 1995;
+    double office_gross = 27.5;
+
+    cout << "The movie " << movie_name 
+    << " came out in " << year_made << ", and had a world wide gross of around $" << office_gross << " million dollars.";
+    cout << "Some of my favourite quotes form the movie include:\n";
+    
+    cout << "How do you get fired on your day off?";
+    cout << "You win some you lose some, but you live. You live to fight another day";
+}

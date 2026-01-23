156fc96a09877ee29bf3e37431ed8543e5950a10
Jordane <uterjordane@gmail.com>
2026-01-23 21:27:47 +0000

    End

---
+++ M2Lab1.cpp
@@ -0,0 +1,40 @@
+/* 
+CSC 134
+M2Lab1 - The Crate Pogram
+Jordane
+1/28/2025
+
+Find volume of crate
+Find cost, customer_price, and profit
+Print everything out
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    // Declare variables
+    // Constant variables
+    const double COST_PER_CUBIC_FOOT = 0.23;
+    const double CHARGE_PER_CUBIC_FOOT = 0.5;
+
+
+
+    // Create Variables
+    double length, width, height, volume; // all in feet
+    // Money variables
+    double cost, customer_price, profit; // all in dollars
+    
+
+
+    // Get Input
+
+    // Calculations
+
+    // Output
+
+
+
+    return 0;
+}
---
+++ M2T2-Uter.cpp
@@ -0,0 +1,41 @@
+// CSC 134
+// M1T2 - Recipt Calculator
+// Jordane
+// 1/26/2025
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    // Declare variables
+    string meal_name = "Sushi Platter";
+    int num_meals;
+    double meal_price = 5.25;  
+    double sub_total;             
+    double tip_amount;
+    double tax_amount;
+    double total_amount;
+
+    //Get user input
+    cout << "Welcome to the CSC 134 Grill." << endl;
+    cout << "Today's special is the " << meal_name << " for $" << meal_price << "." << endl;
+    cout << endl;
+    cout << "How many meals would you like to order? ";
+    cin >> num_meals;
+    cout << endl;
+
+    // Calculate totals
+    sub_total = meal_price * num_meals;
+
+  
+    // Display receipt
+    cout << "YOUR ORDER" << endl << "-------------------------" << endl;
+    cout << "Subtotal: \t\t$" << sub_total << endl;
+
+    return 0;
+
+
+
+
+
+}

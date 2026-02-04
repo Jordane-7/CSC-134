e892fce377538e0fbb1c992ea6508edaa75b5495
Jordane <uterjordane@gmail.com>
2026-02-04 18:56:49 +0000

    Start

--- M2T1-Uter.cpp
+++ M2T1-Uter.cpp
@@ -1,7 +1,37 @@
 // CSC 134
-// M1T2 - Recipt Calculator
+// M1LAB1 - selling items
 // Jordane
-// 2/4/2026
+// 2/2/2026
 
 #include <iostream>
+#include <iomanip> // for decimal places
 using namespace std;
+
+int main() {
+    // declare vairables
+    string name;
+    string item_name = "Burger";
+    int item_count = 100;           // how many we have
+    double item_cost = 2.53;        // price per item
+    double total_cost;
+
+    // Set to 2 decimal places - requires <iomanip>
+    cout << setprecision(2) << fixed;
+
+    // Do the work
+    cout << "What is your name? " << endl;
+    cin >> name;
+    cout << "Welcome to the " << item_name << " store." << endl;
+    cout << "We have " << item_count << " " << item_name << "s" << endl;
+    cout << "They cost $" << item_cost << " each" << endl;
+
+    // Total price
+    total_cost = item_count * item_cost;
+
+    cout << "To buy all of them it will cost $" << total_cost << endl;
+
+    
+
+
+    return 0;
+}

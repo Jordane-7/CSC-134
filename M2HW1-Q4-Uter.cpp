e3860cd7e5d9e71b7b5776751e8a31ea421cbde1
Jordane <uterjordane@gmail.com>
2026-02-14 19:26:51 +0000

    Finish

---
+++ M2HW1-Q4-Uter.cpp
@@ -0,0 +1,38 @@
+// CSC 134
+// M2HW1 - Gold
+// Jordane
+// 2/14/2026
+
+#include <iostream>
+using namespace std;
+
+int main (){
+    string letsGo, school, team, cheerOne, cheerTwo;
+    
+    // Assign base values
+    letsGo = "Let's go ";
+    school = "FTCC";
+    team = "Trojans";
+
+    // Build cheers using concatenation
+    cheerOne = letsGo + school;
+    cheerTwo = letsGo + team;
+
+    // Output (no raw strings used)
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerOne << endl;
+    cout << cheerTwo << endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+    return 0;
+}

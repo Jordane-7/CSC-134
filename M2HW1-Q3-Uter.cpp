2854fc7480bf4fb8adcd427a266f2523e343b32c
Jordane <uterjordane@gmail.com>
2026-02-14 19:23:14 +0000

    Finish

--- M2HW1-Q3-Uter.cpp
+++ M2HW1-Q3-Uter.cpp
@@ -1,12 +1,27 @@
 // CSC 134
 // M2HW1 - Gold
 // Jordane
-// 2/11/2026
+// 2/14/2026
 
 #include <iostream>
 using namespace std;
 
 int main (){
 
-    string name;
-    double acc_balance, amount_depo, amount_with, final_balance;
+    //INPUTS
+    double NumPizza, SlicePizza, TotalVisitors, SliceLeft;
+    cout << "How many pizzas are there? ";
+    cin >> NumPizza;
+    cout << "How many slices are in each pizza? ";
+    cin >> SlicePizza;
+    cout << "How many visitors are there? ";
+    cin >> TotalVisitors;
+   
+    //CALCULATIONS
+    SliceLeft = (NumPizza * SlicePizza) - (TotalVisitors * 3);   
+
+
+    //OUTPUT
+    cout << "\nThere are " << SliceLeft << " slices of pizza left over." << endl;
+    return 0;
+}

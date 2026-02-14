4bbc95c1b999108a99f21a678718978efc2b9324
Jordane <uterjordane@gmail.com>
2026-02-14 19:17:40 +0000

    Start

---
+++ M2HW1-Q3-Uter.cpp
@@ -0,0 +1,12 @@
+// CSC 134
+// M2HW1 - Gold
+// Jordane
+// 2/11/2026
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+    string name;
+    double acc_balance, amount_depo, amount_with, final_balance;

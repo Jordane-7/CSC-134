d65874555860d9ae25c9da28cd6bf3239285dd80
Jordane <uterjordane@gmail.com>
2026-02-23 16:03:20 +0000

    INPUT

--- M3Lab1.cpp
+++ M3Lab1.cpp
@@ -1,4 +1,33 @@
 // CSC 134
-// M1LAB1 - selling items
+// M3lab1 - Option game
 // Jordane
 // 2/23/2026
+
+#include <iostream>
+using namespace std;
+
+void do_choice_1 ();
+void do_choice_2 ();
+void do_choice_3 ();
+void do_choice_4 ();
+
+
+int main() {
+    int choice;
+
+    cout << "You're currenlty second place in a drag race" << endl;
+    cout << "Please choose an option (1, 2, or 3): ";
+    cin >> choice;
+
+    if (choice == 1) {
+        do_choice_1();
+    } else if (choice == 2) {
+        do_choice_2();
+    } else if (choice == 3) {
+        do_choice_3();
+    } else {
+        cout << "Invalid choice. Please choose 1, 2, or 3." << endl;
+    }
+
+    return 0;
+}

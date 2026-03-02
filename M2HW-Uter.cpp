15377937dca00606958ba4804ec311434f863f1e
Jordane <uterjordane@gmail.com>
2026-03-02 16:14:10 +0000

    Start

---
+++ M2HW-Uter.cpp
@@ -0,0 +1,42 @@
+// CSC 134
+// M3HW1 - Gold
+// Jordane Uter
+// 3/2/2026
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Question 1
+    cout << "Question 1" << endl;
+
+    cout << "Hello, I'm a C++ program!" << endl;
+    cout << "How are you today? Please type happy, sad, tired, excited, or something else." << endl;
+
+    string user_feeling;
+    cin >> user_feeling;
+
+    if 
+    (user_feeling == "happy") {
+        cout << "That's great to hear! Keep up the positive vibes!" << endl;
+    } 
+    else if (user_feeling == "sad") {
+        cout << "I'm sorry to hear that. I hope your day gets better!" << endl;
+    }
+    else if (user_feeling == "tired") {
+        cout << "I'm sorry to hear that. I hope you get some rest soon!" << endl;
+    } 
+     else if (user_feeling == "excited") {
+        cout << "That's exciting! I hope you enjoy your excitement!" << endl;
+    }
+    else if (user_feeling == "something else") {
+        cout << "I'm sorry to hear that. I hope your day gets better!" << endl;
+    } 
+    } cout << "Thanks for sharing! I hope your day goes well." << endl;
+     else {
+        cout << "I didn't understand that. Please type happy, sad, tired, excited, or something else." << endl;
+    } 
+    
+   
+
+}

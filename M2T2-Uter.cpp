0fd6bc667c432a34212b520d81dcb9e255dcd501
Jordane <uterjordane@gmail.com>
2026-03-18 14:05:34 +0000

    final

--- M2T2-Uter.cpp
+++ M2T2-Uter.cpp
@@ -4,6 +4,7 @@
 // 1/26/2026
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main () {
@@ -16,7 +17,10 @@ int main () {
     double tax_amount;
     double total_amount;
 
-    //Get user input
+    double tax_rate = 0.08;
+    double tip_rate = 0.15;
+
+    // Get user input
     cout << "Welcome to the CSC 134 Grill." << endl;
     cout << "Today's special is the " << meal_name << " for $" << meal_price << "." << endl;
     cout << endl;
@@ -26,16 +30,20 @@ int main () {
 
     // Calculate totals
     sub_total = meal_price * num_meals;
+    tax_amount = sub_total * tax_rate;
+    tip_amount = sub_total * tip_rate;
+    total_amount = sub_total + tax_amount + tip_amount;
 
-  
+    // Format to 2 decimal places
+    cout << fixed << setprecision(2);
+
     // Display receipt
     cout << "YOUR ORDER" << endl << "-------------------------" << endl;
-    cout << "Subtotal: \t\t$" << sub_total << endl;
+    cout << "Subtotal:\t\t$" << sub_total << endl;
+    cout << "Tax (8%):\t\t$" << tax_amount << endl;
+    cout << "Tip (15%):\t\t$" << tip_amount << endl;
+    cout << "Total:\t\t\t$" << total_amount << endl;
 
     return 0;
-
-
-
-
 
 }
---
+++ M4LAB1-Uter.cpp
@@ -0,0 +1,18 @@
+// CSC 134
+// M4T1
+// Jordane
+// 3/6/2026
+
+#include <iostream>
+using namespace std;
+
+int main (){
+
+
+
+
+
+
+
+    
+}

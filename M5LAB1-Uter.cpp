a8618fc989844d31942145b1d48910091a495e03
Jordane <uterjordane@gmail.com>
2026-03-25 15:38:22 +0000

    start

---
+++ M5LAB1-Uter.cpp
@@ -0,0 +1,21 @@
+// CSC 134
+// M4T1
+// Jordane
+// 3/25/2026
+
+#include <iostream>
+using namespace std;
+
+void do_choice_1();
+void do_choice_2();
+void do_choice_3();
+void do_choice_4();
+
+int main (){
+
+
+
+
+
+
+}

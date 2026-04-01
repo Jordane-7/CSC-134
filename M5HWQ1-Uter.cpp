aced80551ae093b7f2d2f55ebcd4be5eacafe55b
Jordane <uterjordane@gmail.com>
2026-04-01 14:33:57 +0000

    Start

---
+++ M5HWQ1-Uter.cpp
@@ -0,0 +1,35 @@
+// CSC 134
+// M5HW1
+// Jordane
+// 4/1/2026
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main () {
+    string month_1, month_2, month_3;
+    double rain_1, rain_2, rain_3, average;
+
+    cout << "Welcome to the Rainfall Average Calculator!" << endl;
+    cout << "Please enter the first month and the amount of rainfall." << endl;
+
+    cout << "Month: " << endl;
+    cin >> month_1; 
+    cout << "Rainfall: "
+    cin >> rain_1;
+
+    cout << "Month: " << endl;
+    cin >> month_2; 
+    cout << "Rainfall: "
+    cin >> rain_2;
+
+    cout << "Month: " << endl;
+    cin >> month_3; 
+    cout << "Rainfall: "
+    cin >> rain_3;
+
+
+
+
+}
